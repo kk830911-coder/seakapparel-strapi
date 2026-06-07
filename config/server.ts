f05271bee481b5c7005144f83d3758b8fa510a1c
kk830911-coder <kk830911@gmail.com>
2026-06-07 17:03:09 +0800

    fix: integrate cloudinary integration and upgrade csp whitelist

--- config/server.ts
+++ config/server.ts
@@ -9,3 +9,15 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Server =>
 });
 
 export default config;
+
+
+export default ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  cors: {
+    enabled: true,
+    origin: ['https://seakapparel.com', 'https://www.seakapparel.com'],
+    headers: ['*'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  },
+});
