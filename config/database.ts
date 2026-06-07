c84c56bbc8d2729c2ef48d984ef7cf1de5c95829
kk830911-coder <kk830911@gmail.com>
2026-06-07 14:45:37 +0800

    setup strapi for render

--- config/database.ts
+++ config/database.ts
@@ -1,9 +1,28 @@
-export default ({ env }) => ({
-  connection: {
-    client: 'sqlite',
+// config/database.js
+module.exports = ({ env }) => {
+  if (env('NODE_ENV') === 'production') {
+    return {
+      connection: {
+        client: 'postgres',
+        connection: {
+          connectionString: env('DATABASE_URL'),
+          ssl: { rejectUnauthorized: false },
+        },
+        options: {
+          ssl: true,
+        },
+      },
+    };
+  }
+
+  // 本地开发依然用 SQLite
+  return {
     connection: {
-      filename: env('DATABASE_FILENAME', '/tmp/data.db'),
+      client: 'sqlite',
+      connection: {
+        filename: '.tmp/data.db',
+      },
+      useNullAsDefault: true,
     },
-    useNullAsDefault: true,
-  },
-});
+  };
+};
