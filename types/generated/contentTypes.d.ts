bb6b0f7444cdcbceed51a3cbe9930f4cad384d4f
kk830911 <kk830911@gmail.com>
2026-06-16 19:47:33 +0800

    fix: resolve key issues and correct cors middleware configuration

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -512,6 +512,12 @@ export interface ApiBlogBlog extends Struct.CollectionTypeSchema {
           localized: true;
         };
       }>;
+    cover_url: Schema.Attribute.String &
+      Schema.Attribute.SetPluginOptions<{
+        i18n: {
+          localized: true;
+        };
+      }>;
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
